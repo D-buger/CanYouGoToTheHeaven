8292943c154109d9b997fa991fc378887ded5df9
D-buger <o3oduck@naver.com>
2021-08-16 23:19:38 +0900

    물관련 효과으어으어 아직 미완

--- Assets/01. Scripts/SOO/Effect/TextEffects.cs
+++ Assets/01. Scripts/SOO/Effect/TextEffects.cs
@@ -2,16 +2,79 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public static class TextEffects
 {
-    static IEnumerator cTyping(Text _text, string _message, float _typingTime)
+    public static IEnumerator Waving(TMP_TextInfo textInfo)
+    {
+        TMP_CharacterInfo charInfo;
+        for (int i = 0; i < textInfo.characterCount; ++i)
+        {
+            charInfo = textInfo.characterInfo[i];
+
+            if (!charInfo.isVisible)
+                continue;
+
+            TMP_MeshInfo meshInfo = textInfo.meshInfo[charInfo.materialReferenceIndex];
+
+            for (int j = 0; j < 4; ++j)
+            {
+                int index = charInfo.vertexIndex + j;
+                Vector3 vec = meshInfo.vertices[index];
+                meshInfo.vertices[index] = vec +
+                    new Vector3(0, Mathf.Sin(Time.time * 3f + vec.x * 1f) * 0.1f, 0);
+            }
+            yield return null;
+        }
+    }
+
+    public static IEnumerator Typing(Text _text, string _message, float _typingTime)
     {
         yield return new WaitForSeconds(2f);
         for (int i = 0; i < _message.Length; i++)
         {
             _text.text = _message.Substring(0, i);
             yield return new WaitForSeconds(_typingTime);
+        }
+    }
+
+    public static void Gradient(TMP_TextInfo textInfo, Gradient gradient)
+    {
+        TMP_CharacterInfo charInfo;
+
+        float min = textInfo.characterInfo[0].vertex_BL.position.x;
+        float max = textInfo.characterInfo[textInfo.characterCount - 1].vertex_TR.position.x;
+        for (int i = 0; i < textInfo.characterCount; ++i)
+        {
+            charInfo = textInfo.characterInfo[i];
+
+            if (!charInfo.isVisible)
+                continue;
+
+            TMP_MeshInfo meshInfo = textInfo.meshInfo[charInfo.materialReferenceIndex];
+
+            for (int j = 0; j < 4; ++j)
+            {
+                int index = charInfo.vertexIndex + j;
+                Vector3 vec = meshInfo.vertices[index];
+                float curXNormalized =
+                       Mathf.InverseLerp(min, max, vec.x);
+                Color color = gradient.Evaluate(curXNormalized);
+                meshInfo.colors32[index] = new Color(color.r, color.g, color.b, 1);
+            }
+        }
+    }
+
+    public static void UpdateTexts(TMP_Text textComponent)
+    {
+        TMP_TextInfo textInfo = textComponent.textInfo;
+        for (int i = 0; i < textInfo.meshInfo.Length; ++i)
+        {
+            TMP_MeshInfo meshInfo = textInfo.meshInfo[i];
+            meshInfo.mesh.vertices = meshInfo.vertices;
+            meshInfo.mesh.colors32 = meshInfo.colors32;
+            textComponent.UpdateGeometry(meshInfo.mesh, i);
         }
     }
 }
--- Assets/01. Scripts/SOO/Effect/TextInput.cs
+++ Assets/01. Scripts/SOO/Effect/TextInput.cs
@@ -30,46 +30,22 @@ public class TextInput : MonoBehaviour
     private void Update()
     {
         textComponent.ForceMeshUpdate();
-        Test(textComponent.textInfo);
+        TextEffects.UpdateTexts(textComponent);
     }
 
-    private void Test(TMP_TextInfo textInfo)
+    private void MoveEffect(IEnumerator effect)
     {
-        TMP_CharacterInfo charInfo;
-
-        float min = textInfo.characterInfo[0].vertex_BL.position.x;
-        float max = textInfo.characterInfo[textInfo.characterCount - 1].vertex_TR.position.x;
-
-        for (int i = 0; i < textInfo.characterCount; ++i)
-        {
-            charInfo = textInfo.characterInfo[i];
-
-            if (!charInfo.isVisible)
-                continue;
-
-            TMP_MeshInfo meshInfo = textInfo.meshInfo[charInfo.materialReferenceIndex];
-            
-            for(int j = 0; j < 4; ++j)
-            {
-                int index = charInfo.vertexIndex + j;
-                Vector3 vec = meshInfo.vertices[index];
-                meshInfo.vertices[index] = vec +
-                    new Vector3(0, Mathf.Sin(Time.time * 3f + vec.x * 1f) * 0.1f, 0);
-                float curXNormalized = 
-                    Mathf.InverseLerp(min, max, vec.x);
-                Color color = gradient.Evaluate(curXNormalized);
-                meshInfo.colors32[index] = new Color(color.r, color.g, color.b, 1);
-            }
-        }
-
-        for(int i = 0; i < textInfo.meshInfo.Length; ++i)
-        {
-            TMP_MeshInfo meshInfo = textInfo.meshInfo[i];
-            meshInfo.mesh.vertices = meshInfo.vertices;
-            meshInfo.mesh.colors32 = meshInfo.colors32;
-            textComponent.UpdateGeometry(meshInfo.mesh, i);
-        }
+        if (moveEffect != null)
+            StopCoroutine(moveEffect);
+        moveEffect = effect;
+        StartCoroutine(moveEffect);
     }
 
-
+    private void ColorEffect(IEnumerator effect)
+    {
+        if (colorEffect != null)
+            StopCoroutine(colorEffect);
+        colorEffect = effect;
+        StartCoroutine(colorEffect);
+    }
 }
