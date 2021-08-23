1f05ca84d9e57398c05f6ddce31d7bec18a8c74b
D-buger <o3oduck@naver.com>
2021-08-23 12:50:13 +0900

    랜덤맵 틀 간단하게 완성

--- Assets/01. Scripts/SOO/LevelGeneration.cs
+++ Assets/01. Scripts/SOO/LevelGeneration.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class LevelGeneration : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameObject[] rooms;
+    [SerializeField]
+    private int roomInStage;
+    [SerializeField]
+    private Tilemap[] tilemaps;
+    private float cellSize;
+
+    private void Start()
     {
-        
+        tilemaps = new Tilemap[rooms.Length];
+        for(int i = 0; i < rooms.Length; i++)
+        {
+            tilemaps[i] = rooms[i].transform.GetChild(0)
+                .transform.GetChild(0).GetComponent<Tilemap>();
+        }
+        cellSize = rooms[0].transform.GetChild(0).GetComponent<Grid>().cellSize.y;
+
+        Generation();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Generation()
     {
-        
+        int random;
+        GameObject room;
+        float ySize;
+        Vector2 nextPos = transform.position;
+
+        for (int i = 0; i < roomInStage; i++)
+        {
+            random = Random.Range(0, rooms.Length);
+            room = Instantiate(rooms[random], nextPos, Quaternion.identity);
+            ySize = tilemaps[random].size.y * cellSize;
+            nextPos = room.transform.GetChild(0).position;
+            nextPos.y += ySize;
+        }
     }
 }
