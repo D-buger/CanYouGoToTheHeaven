32066a8bb2b118ea8876d0d43f342b57aefb5c37
Thirsted <gusalstjr11085@gmail.com>
2021-10-05 07:35:15 +0900

    사망 시 스크린 추가

--- Assets/01Scripts/H/Monobehaviour/UI/GameOverScreen.cs
+++ Assets/01Scripts/H/Monobehaviour/UI/GameOverScreen.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameOverScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Text gameOverMessage;
+    [SerializeField] Image background;
+    [SerializeField] GameObject buttons;
+
+    public void MethodStart()
     {
-        
+        StartCoroutine(AdjustBlackScreen());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator ProgressUI()
     {
-        
+        StartCoroutine(TypingEffect(gameOverMessage, "Game Over", 0.32f));
+        WaitForSeconds waitDelay00 = new WaitForSeconds("Game Over".Length * 0.32f + 1f);
+        yield return waitDelay00;
+
+        buttons.SetActive(true);
+    }
+
+    WaitForEndOfFrame waitForEndOfFrame = new WaitForEndOfFrame();
+
+    IEnumerator AdjustBlackScreen()
+    {
+        float timeCounter = 0f;
+        background.gameObject.SetActive(true);
+
+        while (true)
+        {
+            float deltaTime = Time.deltaTime;
+
+            timeCounter += deltaTime;
+            background.color = new Color(background.color.r, background.color.g, background.color.b, background.color.a + (deltaTime * 0.4f));
+            if (timeCounter >= 4f)
+            {
+                break;
+            }
+            yield return waitForEndOfFrame;
+        }
+        StartCoroutine(ProgressUI());
+    }
+
+    IEnumerator TypingEffect(Text _text, string _message, float _speed)
+    {
+        WaitForSeconds ddd = new WaitForSeconds(_speed);
+        for (int i = 0; i < _message.Length; i++)
+        {
+            _text.text = _message.Substring(0, i + 1);
+            yield return ddd;
+        }
     }
 }
